5fa31379f0cbe88d52f429608b544b3054be685c
eric52145 <30455287+eric52145@users.noreply.github.com>
2022-08-27 02:00:25 +0800

    start

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+// import data from './data'
+
+
+export default function App(){
+
+
+    return(
+        <div>
+            <Navbar/>
+            <div className='container'>
+                <Form/>
+
+            </div>
+        </div>
+    )
+};
+    
